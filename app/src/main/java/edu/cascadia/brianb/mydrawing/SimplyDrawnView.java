705c46f8914313a17c4285cb794b9416d893ce80
scottwmaxwell <scott-maxwell@live.com>
2015-10-19 15:07:15 -0700

    In-Class Exercise
    Drew some stuff

--- app/src/main/java/edu/cascadia/brianb/mydrawing/SimplyDrawnView.java
+++ app/src/main/java/edu/cascadia/brianb/mydrawing/SimplyDrawnView.java
@@ -44,16 +44,31 @@ public class SimplyDrawnView extends View {
 
         //Draw red line
         mPaint.setColor(Color.RED); mPaint.setStrokeWidth(10);
-
+        canvas.drawLine(mWidth / 2, mHeight / 2, mWidth, mHeight, mPaint);
         //Draw green lines
-
-
+        mPaint.setColor(Color.GREEN); mPaint.setStrokeWidth(20);
+        canvas.drawLine(20, 50, mWidth - 20, 50, mPaint);
+        mPaint.setColor(Color.GREEN); mPaint.setStrokeWidth(20); mPaint.setAlpha(100);
+        canvas.drawLine(20, 200, mWidth - 20, 200, mPaint);
+        mPaint.setColor(Color.GREEN); mPaint.setStrokeWidth(20); mPaint.setAlpha(75);
+        canvas.drawLine(20, 350, mWidth - 20, 350, mPaint);
+        mPaint.setColor(Color.GREEN); mPaint.setStrokeWidth(20); mPaint.setAlpha(50);
+        canvas.drawLine(20, 500, mWidth - 20, 500, mPaint);
+        mPaint.setColor(Color.GREEN); mPaint.setStrokeWidth(20); mPaint.setAlpha(25);
+        canvas.drawLine(20, 650, mWidth - 20, 650, mPaint);
         //Draw Text
-
+        mPaint.setColor(Color.BLACK); mPaint.setTextSize(50);
+        canvas.drawText("I am drawing.", 20, mHeight / 2, mPaint);
         //Draw Text on a Path
-
+        //mPaint.setColor(Color.CYAN); mPaint.setTextSize(60);
+        //canvas.drawTextOnPath("drawing on a Path can be lots of fun", mPath, 150, 150, mPaint);
         //Draw filled, opaque, and open ovals
-
+        mPaint.setColor(Color.RED);
+        canvas.drawCircle(mWidth / 2, mHeight / 2, 20, mPaint);
+        mPaint.setColor(Color.GRAY); mPaint.setAlpha(50);
+        canvas.drawCircle(mWidth / 2, mHeight / 2, 150, mPaint);
+        mPaint.setColor(Color.BLACK); mPaint.setStyle(Paint.Style.STROKE); mPaint.setStrokeWidth(10);
+        canvas.drawCircle(mWidth / 2, mHeight / 2, 150, mPaint);
         //Draw bee bitmap
         Drawable b = getResources().getDrawable(R.drawable.bee,null);
 
